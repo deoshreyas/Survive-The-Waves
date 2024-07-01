31300329377c4c54450092920cd2d57e975d1dc7
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-07-01 12:56:53 +0530

    Improve Enemy AI, Add random enemy spawning, Play with Particle Effects, Fix Bugs

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
+var enemy = preload("res://Scenes/enemy.tscn")
+
 func _ready():
 	Global.node_creation_parent = self 
 
 func _exit_tree():
 	Global.node_creation_parent = null
+
+func _on_enemy_spawn_timer_timeout():
+	var enemy_pos = Vector2(randi_range(-160, 670), randi_range(-90, 390))
+	while enemy_pos.x<640 and enemy_pos.x>-80 or enemy_pos.y<360 and enemy_pos.y>-45:
+		enemy_pos = Vector2(randi_range(-160, 670), randi_range(-90, 390)) 
+	Global.instance_node(enemy, enemy_pos, self)
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,16 +3,33 @@ extends Sprite2D
 const SPEED = 75.0 
 var velocity = Vector2()
 
+var stunned = false
+
+var hp = 3
+
+var blood_particles = preload("res://Scenes/blood_particles.tscn")
+
 func _process(delta):
-	if Global.player!=null:
+	if Global.player!=null and not stunned:
 		velocity = global_position.direction_to(Global.player.global_position)
+	elif stunned:
+		velocity = lerp(velocity, Vector2.ZERO, 0.3)
 	global_position += velocity * SPEED * delta 
+	if hp<=0:
+		if Global.node_creation_parent!=null:
+			var blood_particles_instance = Global.instance_node(blood_particles, global_position, Global.node_creation_parent)
+			blood_particles_instance.rotation = velocity.angle()
+		queue_free()
 
 func _on_hitbox_area_entered(area):
-	if area.is_in_group("Bullets"):
-		$StunTimer.start()
+	if area.is_in_group("Bullets") and not stunned:
 		modulate = Color.WHITE
+		velocity = -velocity * 6
+		stunned = true 
+		$StunTimer.start()
 		area.get_parent().queue_free()
+		hp -= 1 
 
 func _on_stun_timer_timeout():
 	modulate = Color("0028ff")
+	stunned = false
